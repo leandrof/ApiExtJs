e3cd6d354eaba24b2af4c3fe4b94184d2c962159
leandrof <leandrofantinel@gmail.com>
2016-06-09 19:32:01 -0300

    Commit inicial

---
+++ ApiExtJs/src/bt/com/roborg/extjs/RetornoBean.java
@@ -0,0 +1,11 @@
+package bt.com.roborg.extjs;
+
+import java.util.List;
+
+
+public class RetornoBean<T> {
+	public List<T> list;
+	public int totalCount;
+	public boolean success;
+	public String errorMsg;
+}
